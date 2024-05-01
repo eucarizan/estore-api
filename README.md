d398245884d29ea82fb19b565210022e5db0531c
eucarizan <eucarizan@gmail.com>
2024-05-01 23:04:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample Ecommerce App
+This is a sample ecommerce app API from Modern API Development with Spring 6 and Spring Boot 3
+
---
+++ start_spring.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [[ ! -a estore-api.zip ]]; then
+  curl https://start.spring.io/starter.zip -d dependencies=web \
+	  -d bootVersion=3.2.4 -d type=gradle-project \
+	  -d javaVersion=17 -d packaging=jar \
+	  -d groupId=dev.nj -d artifactId=estore-api \
+	  -d name=estore-api -d packageName=dev.nj.estore-api \
+	  -d description="Sample ecommerce app API" \
+	  -o estore-api.zip 
+  unzip estore-api.zip
+  mkdir src/main/resources/api
+  cp openapi.yaml config.json .openapi-generator-ignore src/main/resources/api/
+fi
