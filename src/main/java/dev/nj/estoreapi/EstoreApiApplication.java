d486900093aefda1c1b6e279c9b1b4957b442e6f
eucarizan <eucarizan@gmail.com>
2024-05-02 15:05:26 +0800

    generated project files

---
+++ src/main/java/dev/nj/estoreapi/EstoreApiApplication.java
@@ -0,0 +1,13 @@
+package dev.nj.estoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nj/estoreapi/EstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nj.estoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
