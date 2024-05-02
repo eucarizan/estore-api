cf47509234856c807cab11bb1d69bfd1dde7a61f
eucarizan <eucarizan@gmail.com>
2024-05-02 16:46:22 +0800

    add entities

---
+++ src/main/java/dev/nj/estoreapi/entity/ShipmentEntity.java
@@ -0,0 +1,52 @@
+package dev.nj.estoreapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "shipment")
+public class ShipmentEntity {
+  @Id
+  @GeneratedValue
+  @Column(name = "ID", updatable = false, nullable = false)
+  private UUID id;
+
+  @Column(name = "EST_DELIVERY_DATE")
+  private Timestamp estDeliveryDate;
+
+  @Column(name = "CARRIER")
+  private String carrier;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public ShipmentEntity setId(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  public Timestamp getEstDeliveryDate() {
+    return estDeliveryDate;
+  }
+
+  public ShipmentEntity setEstDeliveryDate(Timestamp estDeliveryDate) {
+    this.estDeliveryDate = estDeliveryDate;
+    return this;
+  }
+
+  public String getCarrier() {
+    return carrier;
+  }
+
+  public ShipmentEntity setCarrier(String carrier) {
+    this.carrier = carrier;
+    return this;
+  }
+
+}
