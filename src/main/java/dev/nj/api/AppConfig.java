c896b5912a43c20d962786129b33e268360b6c93
eucarizan <eucarizan@gmail.com>
2024-05-06 14:42:25 +0800

    added appconfig for etag

---
+++ src/main/java/dev/nj/api/AppConfig.java
@@ -0,0 +1,13 @@
+package dev.nj.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class AppConfig {
+  @Bean
+  public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+    return new ShallowEtagHeaderFilter();
+  }
+}
