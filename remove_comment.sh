54f75c70dff2fa27931db0bdd84495a45bf34bc1
eucarizan <eucarizan@gmail.com>
2024-05-06 14:47:03 +0800

    updated gitignore

--- remove_comment.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-find . -type f -name '*.java' -exec sed -ir 's_^/\*\*\|^\ \*.*__' {} \;
-find . -type f -name '*.java' -exec sed -zir 's_;[\n]*@_;\n\n@_' {} \;
-find . -type f -name '*.java' -exec sed -zir 's_;[\n]*public_;\n\npublic_' {} \;
-rm *.javar
-find . -type f -exec sed -i 's/com.packt.modern/dev.nj/' {} \;
